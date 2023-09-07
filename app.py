3d598c4ec1482eebe1ee9498ed4c515e5007a99c
C-Hurtmann <constantine2903@gmail.com>
2023-09-07 21:54:09 +0300

    create docker-compose to run all stuff

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+from datetime import datetime
+from time import sleep
+
 import os
 import requests
+import tempfile
+import uuid
 
 from celery import Celery
-from flask import Flask, render_template, redirect, request, flash, jsonify, url_for
+from flask import Flask, render_template, redirect, request, url_for, Response
+from flask_sse import sse
 
 
 app = Flask(__name__)
+
 app.config['UPLOAD_DIRECTORY'] = 'uploads/'
 app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
@@ -35,6 +42,15 @@ def upload():
     file_path = upload_file.apply_async(args=[url])
     return redirect(url_for('index'))
 
-
+@app.route('/stream')
+def stream():
+    
+    def get_data():
+        while True:
+            sleep(1)
+            yield f'Data: {datetime.now()}\n\n'
+    return Response(get_data(), mimetype='text/event-stream')
+            
+            
 if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0')
