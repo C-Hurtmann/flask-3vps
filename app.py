abb3e9271a26578b133df516a07f8e2c838e823f
C-Hurtmann <constantine2903@gmail.com>
2023-09-08 19:38:50 +0300

    build front-end

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from datetime import datetime
 from time import sleep
 
 import os
-import requests
-import tempfile
-import uuid
+from werkzeug.utils import secure_filename
 
 from celery import Celery
 from flask import Flask, render_template, redirect, request, url_for, Response
@@ -17,20 +15,19 @@ app.config['UPLOAD_DIRECTORY'] = 'uploads/'
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 
+
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 @celery.task
-def upload_file(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        file_path = os.path.join(
-            app.config['UPLOAD_DIRECTORY'],
-            os.path.basename(url),
-        )
-        with open(file_path, 'wb') as f:
-            f.write(response.content)
-        return file_path
+def upload_file(file_data, file_name):
+    file_path = os.path.join(
+        app.config['UPLOAD_DIRECTORY'],
+        secure_filename(file_name),
+    )
+    with open(file_path, 'wb') as f:
+        f.write(file_data)
+    return file_path
 
 @app.route('/')
 def index():
@@ -38,8 +35,11 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    url = request.form['url']
-    file_path = upload_file.apply_async(args=[url])
+    file = request.files['file']
+    if file:
+        file_data = file.read()
+        file_name = file.filename
+        file_path = upload_file.apply_async(args=[file_data, file_name])
     return redirect(url_for('index'))
 
 @app.route('/stream')
@@ -50,7 +50,7 @@ def stream():
             sleep(1)
             yield f'Data: {datetime.now()}\n\n'
     return Response(get_data(), mimetype='text/event-stream')
-            
-            
+
+
 if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0')
--- static/js/upload.js
+++ static/js/upload.js
@@ -3,26 +3,61 @@ fileInput = form.querySelector(".file-input"),
 progressArea = document.querySelector(".progress-area"),
 uploadedArea = document.querySelector(".uploaded-area");
 
-form.addEventListener("submit", ()=>{
+form.addEventListener("click", ()=>{
     fileInput.click();
 });
 
 fileInput.onchange = ({target}) =>{
-    let fileUrl = target.value // getting url from form
-    if (fileUrl){ // if url is selected
-        let fileName = fileUrl.split("/").pop(); // getting file name. It is a after the last slash in url
-        console.log(fileName);
-        uploadFIle(fileUrl); // calling uploadFile with passing file name as an argument
+    let file = target.files[0] // getting file and [0] this means if user has selected multiplies files then get first one only
+    if (file){ // if url is selected
+        let fileName = file.name; // getting selected file name
+        if (fileName.length >= 12) { // if filename is greater or equal 12 
+            let splitName = fileName.split('.');
+            fileName = splitName[0].substring(0, 12) + "... ." + splitName[1];
+        }
+        uploadFIle(fileName); // calling uploadFile with passing file name as an argument
     }
 }
-/*
-function uploadFIle(url) {
+
+function uploadFIle(name) {
     let xhr = new XMLHttpRequest(); // creating new xml object (AJAX)
     xhr.open("POST", "/upload"); // sending post request to the specified url
-    xhr.upload.addEventListener("progress", e =>{
-        console.log(e);
+    xhr.upload.addEventListener("progress", ({loaded, total}) =>{
+        let fileLoaded = Math.floor((loaded / total * 100)); // getting percentage of loaded file size
+        let fileTotal = Math.floor((total / 1024)) // getting file size in MB from bytes
+        let fileSize;
+        // if file size is less than 1024 then add only KB else convert size to MB
+        (fileTotal < 1024) ? fileSize = (fileTotal + " KB"): fileSize = ((loaded / (1024 * 1024)).toFixed(2) + " MB");
+        let progressHTML = `<li class="row">
+                                <i class="fas fa-file-alt"></i>
+                                <div class="content">
+                                <div class="details">
+                                    <span class="name">${name} * Uploading</span>
+                                    <span class="percent">${fileLoaded}%</span>
+                                </div>
+                                <div class="progress-bar">
+                                    <div class="progress" style="width: ${fileLoaded}%"></div>
+                                </div>
+                                </div>
+                            </li>`;
+        uploadedArea.classList.add("onprogress");
+        progressArea.innerHTML = progressHTML;
+        if (loaded == total) {
+            progressArea.innerHTML = "";
+            let uploadedHTML = `<li class="row">
+                                    <div class="content">
+                                    <i class="fas fa-file-alt"></i>
+                                    <div class="details">
+                                        <span class="name">${name} * Uploaded</span>
+                                        <span class="size">${fileSize}</span>
+                                    </div>
+                                    </div>
+                                    <i class="fas fa-check"></i>
+                                </li>`;
+            uploadedArea.classList.remove("onprogress");
+            uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+        };
     });
     let formData = new FormData(form); // formData is an object to easy send form data
-    xhr.send(formData); // sending form data to php
+    xhr.send(formData); // sending form data to flask
 }
-*/
